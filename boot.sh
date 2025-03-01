4756ff574a8f5f3213ed017d1381d2d8dcd71efc
Justin Stone <ndjstn@users.noreply.github.com>
2025-02-28 21:28:33 -0600

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -37,8 +37,9 @@ sudo pacman -Sy --noconfirm && sudo powerpill -Su --noconfirm && paru -Su --noco
 echo "-> Installing main packages..."
 sudo pacman -S --needed --noconfirm \
   aic94xx-firmware ast-firmware gnome-firmware linux-firmware linux-firmware-bnx2x linux-firmware-qcom \
-  linux-firmware-qlogic linux-firmware-whence wd719x-firmware git wget curl top htop btop gtop \
-  sdl2_ttf siji-ttf ttf-0xproto-nerd ttf-3270-nerd ttf-agave-nerd ttf-anonymouspro-nerd \
+  linux-firmware-qlogic linux-firmware-whence wd719x-firmware git wget curl Fsiji
+  htop btop gtop \
+  sdl2_ttf  ttf-0xproto-nerd ttf-3270-nerd ttf-agave-nerd ttf-anonymouspro-nerd \
   ttf-arimo-nerd ttf-bigblueterminal-nerd ttf-bitstream-vera-mono-nerd ttf-cascadia-code-nerd \
   ttf-cascadia-mono-nerd ttf-cousine-nerd ttf-d2coding-nerd ttf-daddytime-mono-nerd ttf-dejavu \
   ttf-dejavu-nerd ttf-envycoder-nerd ttf-fantasque-nerd ttf-firacode-nerd ttf-font-awesome \
@@ -72,14 +73,14 @@ sudo pacman -S --needed --noconfirm \
   python-coverage python-crashtest python-cryptography python-cssselect python-cycler python-dateutil python-dbus python-debugpy python-decorator \
   python-defusedxml python-distlib python-distro python-docstring-to-markdown python-dulwich python-entrypoints python-executing \
   python-fastjsonschema python-filelock python-findpython python-flask python-flask-babel python-flask-cors python-fonttools \
-  python-future python-gevent python-git-versioner python-gnuradio python-gobject python-google-api-core python-google-auth \
-  python-googleapis-common-protos python-gpgme python-greenlet python-gtts python-gtts-token python-html5lib python-httplib2 \
+ python-gevent python-gnuradio python-gobject python-google-api-core python-google-auth \
+  python-googleapis-common-protos python-gpgme python-greenlet python-gtts  python-html5lib python-httplib2 \
   python-idna python-importlib-metadata python-iniconfig python-installer python-ipython-genutils python-itsdangerous python-jaraco.classes \
   python-jaraco.collections python-jaraco.context python-jaraco.functools python-jaraco.text python-jedi python-jeepney python-jinja \
-  python-jsonschema python-jsonschema-specifications python-keyring python-kiwisolver python-langdetect python-lark-parser python-legacy-cgi \
+  python-jsonschema python-jsonschema-specifications python-keyring python-kiwisolver python-lark-parser python-legacy-cgi \
   python-libtmux python-lockfile python-lsp-black python-lsp-jsonrpc python-lsp-server python-lxml python-mako python-markdown python-markdown-it-py \
   python-markupsafe python-matplotlib python-matplotlib-inline python-mccabe python-mdurl python-more-itertools python-msgpack python-mypy_extensions \
-  python-networkx python-numpy python-oauth2client python-orjson python-packaging python-pandas python-paramiko python-parso python-pathspec \
+  python-networkx python-numpy python-oauth2client python-orjsFon python-packaging python-pandas python-paramiko python-parso python-pathspec \
   python-pbs-installer python-pexpect python-pillow python-pip python-pip-system-certs python-pipx python-pkginfo python-platformdirs python-playwright \
   python-pluggy python-poetry python-poetry-core python-poetry-plugin-export python-pooch python-prompt_toolkit python-protobuf python-psutil \
   python-pure-eval python-pyasn1 python-pyasn1-modules python-pyaudio python-pycodestyle python-pycparser python-pycups \
@@ -90,20 +91,18 @@ sudo pacman -S --needed --noconfirm \
   python-setproctitle python-setuptools python-shellingham python-six python-skia-pathops python-soupsieve python-srt python-stack-data python-thrift \
   python-tomlkit python-tqdm python-traitlets python-trove-classifiers python-typing_extensions python-uc-micro-py python-ujson python-urllib3 python-userpath \
   python-virtualenv python-waitress python-watchdog python-wcwidth python-webencodings python-websockets python-werkzeug python-wheel python-wrapt python-xmltodict \
-  python-yaml python-zipp python-zope-event python-zope-interface python-zstandard python2 python2-debug python312 python312-debug uwsgi-plugin-python\
+  python-yaml python-zipp python-zope-event python-zope-interface python-zstandard uwsgi-plugin-python\
   cups cups-filters cups-pk-helper libcups libcupsfilters obsidian polybar i3 bspwm kitty alacritty ghostty wezterm glow ghostwriter networkmanager \
   firefox vivaldi vscodium ostree tree tree-sitter tree-sitter-c tree-sitter-lua tree-sitter-markdown tree-sitter-query tree-sitter-vim tree-sitter-vimdoc \
-  gnuradio-osmosdr rtl-sdr soapysd cheese zoom ticktick task xorg-fonts-encodings xorg-server xorg-server-common xorg-server-xephyr xorg-server-xvfb \
+  gnuradio-osmosdr rtl-sdr  cheese zoom task xorg-fonts-encodings xorg-server xorg-server-common xorg-server-xephyr xorg-server-xvfb \
   xorg-setxkbmap xorg-xauth xorg-xdpyinfo xorg-xinit xorg-xkbcomp xorg-xmessage xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset xorg-xsetroot \
-  xorg-xwayland xorgproto xsel clipcat python-pyperclip wl-clipboard xclip clamav clamtk clamav-unofficial-sigs metaspoliot kernalshark termshark wireshark-cli wireshark-qt \
-  bettercap bettercap-caplets ettercap ettercap-gtk  soapyrtlsdr r rstudio-desktop obs-studio reminna teams-for-linux python-openai-whisper \
+  xorg-xwayland xorgproto xsel clipcat python-pyperclip wl-clipboard xclip clamav clamtk clamav-unofficial-sigs  \
+  bettercap bettercap-caplets ettercap ettercap-gtk  soapyrtlsdr r rstudio-desktop obs-studio teams-for-linux python-openai-whisper \
   alacritty base-devel bat brightnessctl bspwm clipcat dunst eza feh gvfs-mtp firefox geany git imagemagick jq jgmenu kitty libwebp maim \
   mpc mpd mpv neovim ncmpcpp npm pamixer pacman-contrib papirus-icon-theme picom playerctl polybar polkit-gnome python-gobject redshift rofi rustup \
   sxhkd tmux xclip xdg-user-dirs xdo xdotool xsettingsd xorg-xdpyinfo xorg-xkill xorg-xprop xorg-xrandr xorg-xsetroot xorg-xwininfo yazi zsh \
   zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting ttf-inconsolata ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-terminus-nerd \
-  ttf-ubuntu-mono-nerd ueberzugpp webp-pixbuf-loader paru tdrop eww-git i3lock-color simple-mtpfs gh0stzk-gtk-themes gh0stzk-cursor-qogirr \
-  gh0stzk-icons-beautyline gh0stzk-icons-candy gh0stzk-icons-catppuccin-mocha gh0stzk-icons-dracula gh0stzk-icons-glassy gh0stzk-icons-gruvbox-plus-dark \
-  gh0stzk-icons-hack gh0stzk-icons-luv gh0stzk-icons-sweet-rainbow gh0stzk-icons-tokyo-night gh0stzk-icons-vimix-white gh0stzk-icons-zafiro gh0stzk-icons-zafiro-purple
+  ttf-ubuntu-mono-nerd ueberzugpp webp-pixbuf-loader paru tdrop eww-git i3lock-color simple-mtpfs 
 
 # -----------------------------------------------------------------------------
 # Install X11/Wayland packages
